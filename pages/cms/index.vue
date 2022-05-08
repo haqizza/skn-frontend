375e9f1ea3e13a46acee6750bf1c0c4237ffb175
Muhammad Izzatul Haq <haqizza@gmail.com>
2022-05-08 11:40:20 +0700

    Added CMS Homepage, Login Page

---
+++ pages/cms/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <Topbar
+      :title="pageTitle"
+      :userData="userData"
+    />
+    <div class="px-8 pt-5">
+      <p class="font-semibold text-dark tracking-wider">Selamat Datang</p>
+      <h3 class="font-bold text-5xl my-4">{{ userData.name }}</h3>
+      <div class="grid grid-cols-4 grid-flow-row gap-8 mt-8">
+        <div class="p-4 rounded-lg bg-orange col-span-2 row-span-2">
+
+        </div>
+        <KPICard v-for="item in highlight" :key="item.title" :data="item" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CMSHomePage',
+  layout: 'cms',
+  data() {
+    return{
+      pageTitle: 'Overview',
+      userData: {
+        name: 'John Doe',
+        img: '/image/home.jpg'
+      },
+      highlight: [
+        {
+          title: 'Pages',
+          value: '15'
+        },
+        {
+          title: 'Admins',
+          value: '10'
+        },
+        {
+          title: 'Asset',
+          value: '50'
+        },
+        {
+          title: 'Templates',
+          value: '12'
+        },
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-orange">
+    <Login />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "LoginPage"
+
+}
+</script>
